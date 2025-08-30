96362a858230ede4419db7527077fc58c89b675c
Sahil <dhingrasd451@gmail.com>
2025-08-30 20:09:45 +0530

    Update resume link in footer component

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -24,7 +24,7 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://drive.google.com/file/d/14Rj4RladJzei_CfC1GKod_dxbSjqEvQS/view?usp=sharing"
+            href="https://drive.google.com/file/d/1y0TTap6y51MMVYDVmifFvvs3-xC2Jnw_/view?usp=sharing"
           >
             <ArrowIcon />
             <p className="ml-2 h-7">resume</p>
